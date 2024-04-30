972b1479fb4e3e31976c102d13f1833a5f27f9d8
Gustavo Oliveira <gustavoamorim42@gmail.com>
2024-04-29 22:59:17 -0300

    feat: add initial search component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-import Header from "@/components/header"
+import Header from "@/components/header";
+import Search from "@/components/search";
 
 const Home = () => {
-  return <Header></Header>
-}
+  return (
+    <>
+      <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
