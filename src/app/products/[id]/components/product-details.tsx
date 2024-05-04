dce33ad98bc093cb3a49b8078ca185e64a8baf4d
Gustavo Oliveira <gustavoamorim42@gmail.com>
2024-05-03 21:25:49 -0300

    style: add border radius effects

--- src/app/products/[id]/components/product-details.tsx
+++ src/app/products/[id]/components/product-details.tsx
@@ -40,71 +40,73 @@ const ProductDetails = ({ product, extraProducts }: ProductDetailsProps) => {
     });
 
   return (
-    <div className="p-5">
-      <div className="flex items-center gap-1">
-        {/*Restaurant Image */}
-        <div className="relative h-6 w-6">
-          <Image
-            src={product.restaurant.imageUrl}
-            alt={product.restaurant.name}
-            fill
-            className="rounded-full object-cover"
-          />
-        </div>
-        {/* Restaurant name */}
-        <p className="text-xs text-muted-foreground">
-          {product.restaurant.name}
-        </p>
-      </div>
-      {/* Product text */}
-      <h1 className="font-bold text-xl mb-3 mt-1">{product.name}</h1>
-      {/* Price and quantity*/}
-      <div className="flex justify-between">
-        <div>
-          <div className="flex items-center gap-2">
-            <h2 className="text-xl font-semibold">
-              {formatCurrency(calculateProductTotalPrice(product))}
-            </h2>
-            {product.discountPercentage > 0 && (
-              <DiscountBadge product={product} />
-            )}
+    <div className="relative z-50 mt-[-1.5rem] rounded-tl-3xl rounded-tr-3xl bg-white py-5">
+      <div className="p-5">
+        <div className="flex items-center gap-1">
+          {/*Restaurant Image */}
+          <div className="relative h-6 w-6">
+            <Image
+              src={product.restaurant.imageUrl}
+              alt={product.restaurant.name}
+              fill
+              className="rounded-full object-cover"
+            />
           </div>
-          {/* original price */}
-          <p className="text-muted-foreground">
-            De: {formatCurrency(Number(product.price))}
+          {/* Restaurant name */}
+          <p className="text-xs text-muted-foreground">
+            {product.restaurant.name}
           </p>
         </div>
-        {/* Quantity */}
-        <div className="flex items-center justify-between gap-4">
-          <Button
-            size={"icon"}
-            variant={"ghost"}
-            className="border border-solid border-muted-foreground"
-            onClick={handleDecreaseQuantity}
-          >
-            <ChevronLeftIcon />
-          </Button>
-          <span className="w-4">{quantity}</span>
-          <Button size={"icon"} onClick={handleIncreaseQuantity}>
-            <ChevronRightIcon />
-          </Button>
+        {/* Product text */}
+        <h1 className="font-bold text-xl mb-3 mt-1">{product.name}</h1>
+        {/* Price and quantity*/}
+        <div className="flex justify-between">
+          <div>
+            <div className="flex items-center gap-2">
+              <h2 className="text-xl font-semibold">
+                {formatCurrency(calculateProductTotalPrice(product))}
+              </h2>
+              {product.discountPercentage > 0 && (
+                <DiscountBadge product={product} />
+              )}
+            </div>
+            {/* original price */}
+            <p className="text-muted-foreground">
+              De: {formatCurrency(Number(product.price))}
+            </p>
+          </div>
+          {/* Quantity */}
+          <div className="flex items-center justify-between gap-4">
+            <Button
+              size={"icon"}
+              variant={"ghost"}
+              className="border border-solid border-muted-foreground"
+              onClick={handleDecreaseQuantity}
+            >
+              <ChevronLeftIcon />
+            </Button>
+            <span className="w-4">{quantity}</span>
+            <Button size={"icon"} onClick={handleIncreaseQuantity}>
+              <ChevronRightIcon />
+            </Button>
+          </div>
         </div>
-      </div>
-      {/* Delivery data */}
-      <div className="">
-        {/* Delivery Info */}
-        <DeliveryInfo restaurant={product.restaurant} />
-      </div>
-      {/* Product description */}
-      <div className="mt-6 space-y-3">
-        <h3 className="font-semibold">Sobre</h3>
-        <p className="text-sm text-muted-foreground">{product.description}</p>
-      </div>
-      <div className="mt-6 space-y-3">
-        <h3 className="font-semibold">Sucos</h3>
-        <p className="text-sm">
-          <ProductList products={extraProducts} />
-        </p>
+        {/* Delivery data */}
+        <div className="">
+          {/* Delivery Info */}
+          <DeliveryInfo restaurant={product.restaurant} />
+        </div>
+        {/* Product description */}
+        <div className="mt-6 space-y-3">
+          <h3 className="font-semibold">Sobre</h3>
+          <p className="text-sm text-muted-foreground">{product.description}</p>
+        </div>
+        <div className="mt-6 space-y-3">
+          <h3 className="font-semibold">Sucos</h3>
+          <p className="text-sm">
+            <ProductList products={extraProducts} />
+          </p>
+        </div>
       </div>
     </div>
   );
